fa1a516e7c5669f9b603c30e4bd866a99c1d73e0
Amrendra <amrendra9929@gmail.com>
2025-11-18 01:04:23 +0530

    TravelDiary

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios"
 const BASE_URL = "http://localhost:3000/api"
 
 const axiosInstance = axios.create({
-  baseURL: BASE_URL,
+  baseURL: import.meta.env.VITE_API_URL||BASE_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
